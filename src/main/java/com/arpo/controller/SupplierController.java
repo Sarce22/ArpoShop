e24530465a17fa0c3752d777cb04db364d0647fa
Sebastian Arce <sebastianarce482@gmail.com>
2023-09-16 16:23:48 -0500

    Add Crud Suppliers

--- src/main/java/com/arpo/controller/SupplierController.java
+++ src/main/java/com/arpo/controller/SupplierController.java
@@ -3,8 +3,10 @@ package com.arpo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,14 +26,63 @@ public class SupplierController {
 		 return "supplier/add-supplier";
 	 }
 	 
+	 @GetMapping("/listSupplier")
+	 public String listaSupplier(Model model) {
+		 model.addAttribute("ListSupplier", supplierService.listSuppliers());
+		 return "supplier/listar-supplier";
+	 }
+	 
 	 @PostMapping("/saveSupplier")
-	 public String saveSupplier(@ModelAttribute Supplier supplier, Model model) {
-	     boolean supplierId = supplierService.alReadyExist(supplier.getIdSupplier());
-	     if (supplierId) {
-	         model.addAttribute("errorIdDuplicado", true);
-	     } else {
+	 public String saveSupplier(@ModelAttribute Supplier supplier, Model model) { 
 	         supplierService.save(supplier);
-	     }
 	     return "redirect:/supplier/registroSupplier";
 	 }
+	 
+	 @GetMapping("/admin/updateSupplier/{idSupplier}")
+	 public String showUpdateSupplier(@PathVariable("idSupplier") Long idSupplier, Model model) {
+		 Supplier supplier = supplierService.getById(idSupplier);
+		 model.addAttribute("supplier", supplier);
+		 return "supplier/update-supplier"; 
+	 }
+	 
+	 @PostMapping("/admin/updateSupplier/{idSupplier}")
+	 public String updateSupplier(@PathVariable("idSupplier")Long idSupplier,Supplier supplier,
+			 BindingResult result,Model model){
+		 if(result.hasErrors()){
+			 return "supplier/update-user";
+		 }
+		 
+		 Supplier alreadyExist = supplierService.getById(idSupplier);
+		 if(alreadyExist != null) {
+			 alreadyExist.setNameSupplier(supplier.getNameSupplier());
+			 alreadyExist.setAddress(supplier.getAddress());
+			 alreadyExist.setPhoneNumber(supplier.getPhoneNumber());
+			 
+			 supplierService.save(alreadyExist);
+			 
+			 model.addAttribute("successMessage", "El proveedor ha sido modificado.");
+			 return "redirect:/supplier/listSupplier";
+			 
+		 }
+		 return null;
+	 }
+	 
+	 @GetMapping("/admin/deleteSupplier/{idSupplier}")
+	 public String deleteSupplier(@PathVariable("idSupplier") Long idSupplier,Model model) {
+		 Supplier supplier = supplierService.getById(idSupplier);
+		 supplierService.delete(supplier.getIdSupplier());
+		 model.addAttribute("supplier",supplierService.listSuppliers());
+		 return "redirect:/supplier/listSupplier";
+		 
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 }
