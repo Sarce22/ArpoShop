ac9c2d6e71caf96e3b9951febc2bd1d4ed6d5420
Mariana Portela Escobar <113046729+Mariana1010P@users.noreply.github.com>
2023-09-16 00:45:55 -0500

    Crud completo de usuarios

--- src/main/java/com/arpo/controller/UserController.java
+++ src/main/java/com/arpo/controller/UserController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -58,7 +60,52 @@ public class UserController {
 	        return "usuario/add-user";
 	    }
 
+	    @GetMapping("/admin/editUser/{idUser}")
+		public String showUpdateForm(@PathVariable("idUser") Long idUser, Model model) {
+			User user = userService.getById(idUser);
+			model.addAttribute("usuario", user);
+			return "usuario/update-user";
+	    }
+		
+	    
+	    @PostMapping("/admin/updateUser/{idUser}")
+		public String updateUser(@PathVariable("idUser") Long id, User user, BindingResult result, Model model) {
+	    	if (result.hasErrors()) {
+	            // Si hay errores de validación, muestra la vista de actualización nuevamente con mensajes de error.
+	            return "usuario/update-user";
+	    	}
+
+	        // Busca el usuario existente por su id en la base de datos
+
+	    	User alreadyExist = userService.getById(id);
+	        if (alreadyExist != null) {
+	            // Actualiza los datos del usuario existente con los datos del formulario
+	        	alreadyExist.setName(user.getName());
+	        	alreadyExist.setSurname(user.getSurname());
+	        	alreadyExist.setAddress(user.getAddress());
+	        	alreadyExist.setAge(user.getAge());
+	        	alreadyExist.setEmail(user.getEmail());
+	        	alreadyExist.setPhoneNumber(user.getPhoneNumber());
+	        	alreadyExist.setPassword(user.getPassword());
+
+	            // Guarda los cambios en la base de datos
+	            userService.save(alreadyExist);
+	            
+	            // Redirige a la página de listado de usuarios con un mensaje de éxito
+	            model.addAttribute("successMessage", "El usuario ha sido modificado con éxito.");
+	            return "redirect:/user/listado-usuarios";
+	        }
+			return null;
+		}
 	    
 	    
-	    
+	    @GetMapping("/admin/deleteUser/{idUser}")
+	    public String deleteEmpleado(@PathVariable("idUser") Long idUser, Model model) {
+	 	        User user = userService.getById(idUser);
+	 	        userService.delete(user.getIdUser());
+	 	        model.addAttribute("usuario", userService.listUser());
+	 	        return "redirect:/user/listado-usuarios";
+	    }
+
+   
 }
