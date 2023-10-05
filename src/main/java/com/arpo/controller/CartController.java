86ee434802b07d7b407795d42b959ab288c450d5
Sebastian Arce <sebastianarce482@gmail.com>
2023-10-05 17:16:20 -0500

    html cart

--- src/main/java/com/arpo/controller/CartController.java
+++ src/main/java/com/arpo/controller/CartController.java
@@ -1,7 +1,35 @@
 package com.arpo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.arpo.models.Product;
+import com.arpo.service.CartService;
+
+@Controller
+@RequestMapping("/cart")
 public class CartController {
 	
+	@Autowired
+	private CartService cartService;
 	
+	@GetMapping("/listProductCart")
+	public String listCartProduct(Model model) {
+		model.addAttribute("ListProducts",cartService.listCartProduct());
+		return "cart/cart";	
+	}
+	
+	@PostMapping("/addToCart")
+	public String addToCart(@RequestParam("idProduct") Long idProduct, @RequestParam("cantidad") int cantidad) {
+	    Product product = cartService.getByIdProduct(idProduct);
+	  
+	    cartService.saveProduct(product);
+	    return "redirect:/cart/listProductCart";
+	}
 
+	
 }
---
+++ src/main/java/com/arpo/service/CartService.java
@@ -0,0 +1,34 @@
+package com.arpo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.arpo.models.Product;
+import com.arpo.repository.IProductRepository;
+
+@Service
+public class CartService {
+
+	@Autowired
+	private IProductRepository productRepository;
+	
+	public List<Product> listCartProduct(){
+		return productRepository.findAll();
+	}
+	
+	 public Product getByIdProduct(Long id) {
+		 return productRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Producto no encontrado con ID: " + id));
+	}
+	
+	 public Product saveProduct(Product producto) {
+	        return productRepository.save(producto);
+	}
+	
+	 public void deleteProduct(Long id) {
+		    productRepository.deleteById(id);
+	}
+	
+}
